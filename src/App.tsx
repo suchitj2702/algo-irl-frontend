7df47f7874a9f9617e866129663dde8545ac426a
Suchit Jain <suchit.jain99@gmail.com>
2025-06-22 17:15:22 -0700

    Correct vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { AppRouter } from './components/AppRouter';
 
 export function App() {
